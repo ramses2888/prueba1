8d5bce0b07e08922e20f7e66285c0b17425a7a50
ramses <ramsesarnold2888@gmail.com>
2020-07-06 21:51:23 -0500

    commit inicial

---
+++ JavaScript/js/app.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	var nombre = "javascript";
+	var a = 5;
+	var b = 10;
+	var c = 20;
+
+
+	console.log(nombre)
+
+	nombre = "JavaScript"
+
+
+	document.getElementById("saludo").innerHTML = "¡Hola!" + nombre + "!";
+}
